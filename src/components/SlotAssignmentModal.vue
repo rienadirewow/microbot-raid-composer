7196f25609ca057cd5f64b2778012c274e57d8c9
rienadirewow <rienadirewow@gmail.com>
2025-08-07 22:53:02 +0200

    Fix Vue runtime compilation warning
    - Replace template-based icon components with render functions
    
    - Import h function from Vue for render functions
    
    - Remove runtime compilation dependency for icon components
    
    - Fix warning: Component provided template option but runtime compilation is not supported

--- src/components/SlotAssignmentModal.vue
+++ src/components/SlotAssignmentModal.vue
@@ -139,7 +139,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, h } from 'vue'
 import type { PlayerSlot, PlayerCharacter, Role, WoWClass, TierType, TierLevel } from '@/types'
 import { CLASS_ROLE_RESTRICTIONS } from '@/data/wow-data'
 import Modal from './ui/Modal.vue'
@@ -240,7 +240,7 @@ const roleButtons = computed(() => {
   if (props.isFirstSlot) {
     const characterClass = props.character.class
     const allowedRoles = CLASS_ROLE_RESTRICTIONS[characterClass] || []
-    
+
     return allowedRoles.map((role) => {
       switch (role) {
         case 'tank':
@@ -430,19 +430,28 @@ const handleClearSlot = () => {
   emit('close')
 }
 
-// Simple icon components
-const ShieldIcon = {
-  template:
-    '<svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" /></svg>',
-}
+// Simple icon render functions
+const ShieldIcon = () => h('svg', { class: 'h-4 w-4', fill: 'currentColor', viewBox: '0 0 20 20' }, [
+  h('path', { 
+    'fill-rule': 'evenodd', 
+    d: 'M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z', 
+    'clip-rule': 'evenodd' 
+  })
+])
 
-const HeartIcon = {
-  template:
-    '<svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" /></svg>',
-}
+const HeartIcon = () => h('svg', { class: 'h-4 w-4', fill: 'currentColor', viewBox: '0 0 20 20' }, [
+  h('path', { 
+    'fill-rule': 'evenodd', 
+    d: 'M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z', 
+    'clip-rule': 'evenodd' 
+  })
+])
 
-const SwordIcon = {
-  template:
-    '<svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>',
-}
+const SwordIcon = () => h('svg', { class: 'h-4 w-4', fill: 'currentColor', viewBox: '0 0 20 20' }, [
+  h('path', { 
+    'fill-rule': 'evenodd', 
+    d: 'M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z', 
+    'clip-rule': 'evenodd' 
+  })
+])
 </script>
